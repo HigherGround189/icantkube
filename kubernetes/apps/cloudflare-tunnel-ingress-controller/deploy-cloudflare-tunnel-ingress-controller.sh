116e8de83467e8da819f7a9e0e5c369205507886
HigherGround189 <thehigherground18@gmail.com>
2025-12-23 20:15:11 +0000

    created cloudflare-tunnel-ingress-controller deployment

---
+++ kubernetes/apps/cloudflare-tunnel-ingress-controller/deploy-cloudflare-tunnel-ingress-controller.sh
@@ -0,0 +1,6 @@
+kubeseal --format yaml < cloudflare-credentials-unsealed-secret.yaml > cloudflare-credentials.sealed.yaml 
+kubectl -n cloudflare-tunnel-ingress-controller create -f cloudflare-credentials.sealed.yaml
+
+helm repo add strrl.dev https://helm.strrl.dev
+helm install cloudflare-tunnel-ingress-controller strrl.dev/cloudflare-tunnel-ingress-controller \
+  -f values.yaml -n cloudflare-tunnel-ingress-controller --create-namespace
