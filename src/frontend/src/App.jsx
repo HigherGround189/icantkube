b92f08466f9901710b13ca5d0dd32c3ddc6fb90f
Harish Kanna <mysticemiya@gmail.com>
2025-12-25 22:20:30 +0800

    Added communication between frontend and backend

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,22 +1,37 @@
-import { useReducer } from "react";
-// import { useQuery } from "@tanstack/react-query";
+import { useReducer, useState } from "react";
 
 function changeCount(count, action) {
     switch (action.type) {
-        case "inc": return { value: count.value + 1};
-        case "dec": return { value: count.value - 1 };
-        default: return count;
+        case "inc":
+            return { value: count.value + 1 };
+        case "dec":
+            return { value: count.value - 1 };
+        default:
+            return count;
     }
 }
 
 export default function App() {
-    const [count, dispatch] = useReducer(changeCount, { value: 0 })
+    const [count, dispatch] = useReducer(changeCount, { value: 0 });
+    const [message, setMessage] = useState("");
+
+    async function queryBackend() {
+        const res = await fetch(`http://api.localhost:8000/api/${count.value}`);
+        const data = await res.json();
+        setMessage(data.message);
+    }
 
     return (
         <>
-        <div>Count: {count.value}</div>
-        <button onClick={() => dispatch({ type: "inc" })}>+1</button>
-        <button onClick={() => dispatch({ type: "dec" })}>-1</button>
+            <div>Count: {count.value}</div>
+
+            <button onClick={() => dispatch({ type: "inc" })}>+1</button>
+            <button onClick={() => dispatch({ type: "dec" })}>-1</button>
+
+            <hr />
+
+            <button onClick={queryBackend}>Query backend</button>
+            <div>{message}</div>
         </>
-    )
+    );
 }
