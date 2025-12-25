3fba63b6fd3f3182ba11289055453b180b43a735
Harish Kanna <mysticemiya@gmail.com>
2025-12-25 22:29:53 +0800

    Fixed port issue

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
     const [message, setMessage] = useState("");
 
     async function queryBackend() {
-        const res = await fetch(`http://api.localhost:8000/api/${count.value}`);
+        const res = await fetch(`http://api.localhost/api/${count.value}`);
         const data = await res.json();
         setMessage(data.message);
     }
