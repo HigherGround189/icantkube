23d4be6dc432926ab6021c7d0187bb4449933af3
Harish Kanna <mysticemiya@gmail.com>
2025-12-25 17:48:21 +0800

    Added src directory for source code and experimenting with new deployment for frontend

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useReducer } from "react";
+
+function changeCount(count, action) {
+    switch (action.type) {
+        case "inc": return { value: count.value + 1};
+        case "dec": return { value: count.value - 1 };
+        default: return count;
+    }
+}
+
+export default function App() {
+    const [count, dispatch] = useReducer(changeCount, { value: 0 })
+
+    return (
+        <>
+        <div>Count: {count}</div>
+        <button onClick={() => dispatch({ type: "inc" })}>+1</button>
+        <button onClick={() => dispatch({ type: "dec "})}>-1</button>
+        </>
+    )
+}
