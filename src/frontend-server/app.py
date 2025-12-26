ca2d2efe0d3622ca022cbaf9f896377dfc655af2
Harish Kanna <mysticemiya@gmail.com>
2025-12-26 12:45:01 +0800

    Combined source code of frontend and backend and implemented FastAPI serving frontend

--- src/frontend-server/app.py
+++ src/frontend-server/app.py
@@ -1,23 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.mount("/assets", StaticFiles(directory="dist/assets"), name="assets")
-
-@app.get("/")
-def index():
-    from fastapi.responses import FileResponse
-    return FileResponse("dist/index.html")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/api/{count}")
 def echo(count: int):
@@ -27,3 +11,9 @@ def echo(count: int):
         return {"message": "You're a positive man!"}
     else:
         return {"message": "You're a negative man!"}
+
+app.mount(
+    "/", 
+    StaticFiles(directory="dist", html=True), 
+    name="vite"
+)
---
+++ src/frontend-server/src/App.jsx
@@ -0,0 +1,37 @@
+import { useReducer, useState } from "react";
+
+function changeCount(count, action) {
+    switch (action.type) {
+        case "inc":
+            return { value: count.value + 1 };
+        case "dec":
+            return { value: count.value - 1 };
+        default:
+            return count;
+    }
+}
+
+export default function App() {
+    const [count, dispatch] = useReducer(changeCount, { value: 0 });
+    const [message, setMessage] = useState("");
+
+    async function queryBackend() {
+        const res = await fetch(`/api/${count.value}`);
+        const data = await res.json();
+        setMessage(data.message);
+    }
+
+    return (
+        <>
+            <div>Count: {count.value}</div>
+
+            <button onClick={() => dispatch({ type: "inc" })}>+1</button>
+            <button onClick={() => dispatch({ type: "dec" })}>-1</button>
+
+            <hr />
+
+            <button onClick={queryBackend}>Query backend</button>
+            <div>{message}</div>
+        </>
+    );
+}
