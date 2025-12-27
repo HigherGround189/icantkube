0fe9cee463ce78de6c7a87adfcf409c492a44557
HigherGround189 <thehigherground18@gmail.com>
2025-12-27 23:38:03 +0800

    added redeploy script in case we need to redeploy everything again

---
+++ kubernetes/redeploy.sh
@@ -0,0 +1,21 @@
+cd /kubernetes/apps/argocd/helm
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd -f values-override.yaml -n argocd --create-namespace
+
+cd /kubernetes/apps/sealed-secrets/helm
+helm install sealed-secrets sealed-secrets/sealed-secrets -f values-override.yaml -n sealed-secrets --create-namespace
+
+cd /kubernetes/data-sources/
+kubeseal --format < github-repo-unsealed-secret.yaml > github-repo.sealed.yaml
+k apply -f github-repo.sealed.yaml
+
+cd /kubernetes/apps/cloudflare-tunnel-ingress-controller
+kubeseal --format yaml < cloudflare-credentials-unsealed-secret.yaml > cloudflare-credentials.sealed.yaml
+
+cd /kubernetes/apps/mlflow
+kubeseal --format yaml < mlflow-auth-credential-unsealed-secret > mlflow-auth-credential.sealed.yaml
+kubeseal --format yaml < mlflow-postgresql-credentials-unsealed-secret.yaml > mlflow-postgresql-credentials.sealed.yaml
+
+cd /kubernetes/argocd-boostrap/
+k apply -f track-create-apps-and-data-sources.yaml
+
