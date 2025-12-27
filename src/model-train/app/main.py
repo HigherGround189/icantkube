e11f5ee93e8a5e458162d27898d9e62aa789f863
TheRealAppleBoi <leongjunhoe88@gmail.com>
2025-12-27 18:26:34 +0800

    add counter for frontend testing

--- src/model-train/app/main.py
+++ src/model-train/app/main.py
@@ -2,13 +2,19 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+counter = 0
+
 @app.route('/start', methods=["POST"])
 def start_training():
-    return jsonify({"action":'initiate training...'})
+    return jsonify({'trackingId':1})
 
 @app.route('/status/<int:trackingId>', methods=["GET"])
 def retrieve_status(trackingId: int):
-    return jsonify({'trackingId':trackingId, 'status':'completed'})
+    global counter
+    counter += 1
+    if counter % 5 == 0:
+        return jsonify({'status':'completed', 'result':{'accuracy':100}})
+    return jsonify({'status':'running', 'progress':counter%5*20})
 
 if __name__=="__main__":
     app.run(port=80)
