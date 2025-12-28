8099723d3da30652e35d3666f074251421a6882f
Harish Kanna <mysticemiya@gmail.com>
2025-12-28 21:25:40 +0800

    Testing health of sample microservice with new health route

--- src/sample-echo-microservice/app/main.py
+++ src/sample-echo-microservice/app/main.py
@@ -5,3 +5,8 @@ app = FastAPI(title="sample-echo-microservice", redirect_slashes=False)
 @app.get("/{msg}")
 def echo(msg: str):
     return {"message": msg}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
