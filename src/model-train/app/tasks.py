c8e0feb6308e41380a69e9f22456337046e1f5aa
TheRealAppleBoi <leongjunhoe88@gmail.com>
2025-12-31 19:19:40 +0800

    Rename model-training-pipeline.py to model_training_pipeline.py

--- src/model-train/app/tasks.py
+++ src/model-train/app/tasks.py
@@ -1,8 +1,11 @@
 from celery import Celery
+from model_training_pipeline import ModelTrainingPipeline
 from time import sleep
 
+# app = Celery('tasks', broker='redis-master.redis.svc.cluster.local/0', backend='redis-master.redis.svc.cluster.local/1')
 app = Celery('tasks', broker='redis://localhost:6370/0', backend='redis://localhost:6370/1')
-@app.task
+
+@app.task()
 def add(x, y):
     i = 0
     while i < 100:
