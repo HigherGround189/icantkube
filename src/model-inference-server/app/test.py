1c0cde34b84e578fce0698baedcef6ad4e4ce251
HigherGround189 <thehigherground18@gmail.com>
2026-02-03 15:16:22 +0800

    updated testing

--- src/model-inference-server/app/test.py
+++ src/model-inference-server/app/test.py
@@ -8,21 +8,13 @@ print("Client initialised")
 registered_models = client.search_registered_models()
 print(registered_models)
 
+model_name = "IrisPipelineModel"
+model_version_details = client.get_model_version(name=model_name, version="1")
+model_uri = model_version_details.source 
+print(f"Model URI for version 1: {model_uri}")
 
-# client = MlflowClient()
-# model_name = "IrisPipelineModel"
-# print("Client initialised")
+model = mlflow.pyfunc.load_model(model_uri)
+print("Model loaded")
 
-# modles = client.search_registered_models()
-# print(modles)
-# # Retrieve details for a specific version
-# model_version_details = client.get_model_version(name=model_name, version="1")
-# # The 'source' attribute contains the full artifact URI
-# model_uri = model_version_details.source 
-# print(f"Model URI for version 1: {model_uri}")
-
-# model = mlflow.pyfunc.load_model("models:/IrisPipelineModel/Version 1")
-# print("Model loaded")
-
-# predictions = model.predict({"5.4","3.4","1.5","0.4","setosa"})
-# print(predictions)
+predictions = model.predict({"5.4","3.4","1.5","0.4","setosa"})
+print(predictions)
