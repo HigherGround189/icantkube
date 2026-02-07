20e8edb3727226c929c3bd4214cc46415ed58f85
TheRealAppleBoi <leongjunhoe88@gmail.com>
2026-02-08 00:30:13 +0800

    Move training_template and fix the self issue

--- src/model-train/app/model_training_pipeline.py
+++ src/model-train/app/model_training_pipeline.py
@@ -32,6 +32,80 @@ from app.config import load_apps
 
 APPS = load_apps()
 
+def training_template(func):
+    """
+    Training skeleton to replicate the procedure from loading data to model training and 
+    evaluation to logging metrics and artifacts
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            self.update(status=Status.RUNNING.value, progress=0)
+
+            df, X, y, _ = func(self, *args, **kwargs) # Retrieve Features and label
+            self.update(progress=10)
+
+            # Split into train and test dataset
+            X_train, X_test, y_train, y_test = self.data_preparation(X, y)
+            self.update(progress=30)
+
+            # Retrieve pipeline
+            _, _, _, pipeline = func(self, *args, **kwargs)
+            self.update(progress=50)
+            
+            # Start mlflow and set experiment
+            run_context = nullcontext()
+            if self.mlflow_enabled:
+                mlflow.set_experiment(self.cfg.experiment_name)
+                run_context = mlflow.start_run(f"{self.cfg.pipeline_name}-{self.trackingId}")
+            
+            with run_context:
+                pipeline.fit(X_train, y_train) # Train model
+                self.update(progress=70)
+
+                y_pred = pipeline.predict(X_test) # Predict output
+                self.update(progress=80)
+
+                acc = self.metrics(y_pred, y_test) # Calculate metrics
+                self.update(progress=90)
+
+                # Log into mlflow
+                signature = infer_signature(X_test, y_pred)
+                if self.mlflow_enabled:
+                    mlflow.log_metric("accuracy", acc)
+                    mlflow.sklearn.log_model(
+                        sk_model=pipeline,
+                        name=self.cfg.model_name,
+                        registered_model_name=self.cfg.registered_model_name,
+                        signature=signature
+                    )
+                    if self.sample_dataset:
+                        dataset = from_pandas(
+                                df,
+                                source="sklearn.datasets.load_iris()",
+                                name="iris_sample_dataset",
+                            )
+                    else:
+                        csv_buffer = BytesIO()
+                        df.to_csv(csv_buffer, index=False)
+                        csv_bytes = csv_buffer.getvalue()
+                        mlflow.log_bytes(csv_bytes, f"{self.cfg.model_name}.csv", artifact_path="datasets")
+                        dataset = from_pandas(df)
+                    
+                    mlflow.log_input(dataset, context="training")
+                    self.rustfs_enabled.delete_object(Bucket=self.bucket_name, Key=self.object_key)
+
+                self.update(status=Status.COMPLETED.value, 
+                            progress=100, 
+                            result=json.dumps({"accuracy": acc})
+                            )
+
+        except Exception as e:
+            self.update(status=Status.FAILED.value, 
+                        error=f"An error occurred: {e}"
+                        )
+    return wrapper
+
 class ModelTrainingPipeline():
     def __init__(self, 
                  update, 
@@ -102,80 +176,6 @@ class ModelTrainingPipeline():
             self.update(status=Status.FAILED.value, 
                         error=f"An error occurred: Failed to download object {self.bucket_name}/{self.object_key}\n{e}"
                         )
-    
-    def training_template(func):
-        """
-        Training skeleton to replicate the procedure from loading data to model training and 
-        evaluation to logging metrics and artifacts
-        """
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            try:
-                self.update(status=Status.RUNNING.value, progress=0)
-
-                df, X, y, _ = func(*args, **kwargs) # Retrieve Features and label
-                self.update(progress=10)
-
-                # Split into train and test dataset
-                X_train, X_test, y_train, y_test = self.data_preparation(X, y)
-                self.update(progress=30)
-
-                # Retrieve pipeline
-                _, _, _, pipeline = func(*args, **kwargs)
-                self.update(progress=50)
-                
-                # Start mlflow and set experiment
-                run_context = nullcontext()
-                if self.mlflow_enabled:
-                    mlflow.set_experiment(self.cfg.experiment_name)
-                    run_context = mlflow.start_run(f"{self.cfg.pipeline_name}-{self.trackingId}")
-                
-                with run_context:
-                    pipeline.fit(X_train, y_train) # Train model
-                    self.update(progress=70)
-
-                    y_pred = pipeline.predict(X_test) # Predict output
-                    self.update(progress=80)
-
-                    acc = self.metrics(y_pred, y_test) # Calculate metrics
-                    self.update(progress=90)
-
-                    # Log into mlflow
-                    signature = infer_signature(X_test, y_pred)
-                    if self.mlflow_enabled:
-                        mlflow.log_metric("accuracy", acc)
-                        mlflow.sklearn.log_model(
-                            sk_model=pipeline,
-                            name=self.cfg.model_name,
-                            registered_model_name=self.cfg.registered_model_name,
-                            signature=signature
-                        )
-                        if self.sample_dataset:
-                            dataset = from_pandas(
-                                    df,
-                                    source="sklearn.datasets.load_iris()",
-                                    name="iris_sample_dataset",
-                                )
-                        else:
-                            csv_buffer = BytesIO()
-                            df.to_csv(csv_buffer, index=False)
-                            csv_bytes = csv_buffer.getvalue()
-                            mlflow.log_bytes(csv_bytes, f"{self.cfg.model_name}.csv", artifact_path="datasets")
-                            dataset = from_pandas(df)
-                        
-                        mlflow.log_input(dataset, context="training")
-                        self.rustfs_enabled.delete_object(Bucket=self.bucket_name, Key=self.object_key)
-
-                    self.update(status=Status.COMPLETED.value, 
-                                progress=100, 
-                                result=json.dumps({"accuracy": acc})
-                                )
-
-            except Exception as e:
-                self.update(status=Status.FAILED.value, 
-                            error=f"An error occurred: {e}"
-                            )
-        return wrapper
 
     @training_template
     def model_train_sample(self) -> Tuple[pd.DataFrame, pd.Series, Pipeline]:
