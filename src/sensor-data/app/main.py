43941d957db4f291d69ce8fe5951933f4007e221
TrialPixie <ngyikheng04@gmail.com>
2026-02-10 14:06:36 +0800

    Rearranging Code Sensor Data

--- src/sensor-data/app/main.py
+++ src/sensor-data/app/main.py
@@ -17,6 +17,68 @@ S3_ENDPOINT = "http://rustfs"
 BUCKET = "datasets"
 REDIS_KEY_PREFIX = "csv_last_index"
 
+CSV_DATA: dict[str, list[dict]] = {}
+lock = threading.Lock()
+
+
+def connect_redis(db=0):
+    candidates = [
+        {"host": "redis", "port":6379} 
+    ]
+    for i, cfg in enumerate(candidates):
+        try:
+            r = redis.Redis(
+                **cfg,
+                socket_connect_timeout=5,
+                socket_timeout=5,
+                decode_responses=True,
+                db=db
+            )
+            response = r.ping()
+            if response:
+                logger.info(f"Connected to Redis Successfully at {cfg['host']}:{cfg['port']}")
+                return r
+        except redis.ConnectionError as conerr:
+            logger.warning(f"Failed to connect to Redis at {cfg['host']}:{cfg['port']}: {conerr}")
+            if i < len(candidates) - 1:
+                logger.info("Trying next redis candidate...")
+
+    logger.warning("Redis unavailable, continuing without tracking")
+    return None
+
+r = connect_redis()
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=S3_ENDPOINT,
+    aws_access_key_id=os.environ.get("access_key_id"),
+    aws_secret_access_key=os.environ.get("secret_access_key"),
+)
+
+def redis_key(name: str) -> str:
+    return f"{REDIS_KEY_PREFIX}:{name}"
+
+
+def get_last_index(name: str, size: int) -> int:
+    if not r:
+        return random.randint(0, size - 1)
+
+    key = redis_key(name)
+    val = r.get(key)
+
+    if val is None:
+        idx = random.randint(0, size - 1)
+        r.set(key, idx)
+        return idx
+
+    return int(val)
+
+
+
+def set_last_index(name: str, idx: int):
+    if r:
+        r.set(redis_key(name), idx)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
@@ -62,68 +124,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="CSV with Redis + RustFS",
               lifespan=lifespan)
-
-CSV_DATA: dict[str, list[dict]] = {}
-lock = threading.Lock()
-
-
-def connect_redis(db=0):
-    candidates = [
-        {"host": "redis", "port":6379} 
-    ]
-    for i, cfg in enumerate(candidates):
-        try:
-            r = redis.Redis(
-                **cfg,
-                socket_connect_timeout=5,
-                socket_timeout=5,
-                decode_responses=True,
-                db=db
-            )
-            response = r.ping()
-            if response:
-                logger.info(f"Connected to Redis Successfully at {cfg["host"]}:{cfg["port"]}")
-                return r
-        except redis.ConnectionError as conerr:
-            logger.warning(f"Failed to connect to Redis at {cfg["host"]}:{cfg["port"]}: {conerr}")
-            if i < len(candidates) - 1:
-                logger.info("Trying next redis candidate...")
-
-    logger.warning("Redis unavailable, continuing without tracking")
-    return None
-
-r = connect_redis()
-
-s3 = boto3.client(
-    "s3",
-    endpoint_url=S3_ENDPOINT,
-    aws_access_key_id=os.environ.get("access_key_id"),
-    aws_secret_access_key=os.environ.get("secret_access_key"),
-)
-
-def redis_key(name: str) -> str:
-    return f"{REDIS_KEY_PREFIX}:{name}"
-
-
-def get_last_index(name: str, size: int) -> int:
-    if not r:
-        return random.randint(0, size - 1)
-
-    key = redis_key(name)
-    val = r.get(key)
-
-    if val is None:
-        idx = random.randint(0, size - 1)
-        r.set(key, idx)
-        return idx
-
-    return int(val)
-
-
-
-def set_last_index(name: str, idx: int):
-    if r:
-        r.set(redis_key(name), idx)
 
 
 @app.get("/get_next_line")
