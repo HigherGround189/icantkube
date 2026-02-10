3dc4691345331a7d264e48eb9ed23e73a7b8bfd8
HigherGround189 <thehigherground18@gmail.com>
2026-02-10 15:32:08 +0800

    added http to endpoints

--- src/model-inference-server/app/main.py
+++ src/model-inference-server/app/main.py
@@ -1,8 +1,8 @@
 import mlflow
 import requests
 
-MACHINE_DATA_ENDPOINT = "machines-data/all"
-SENSOR_DATA_ENDPOINT = "sensor-data-service/get_next_line"
+MACHINE_DATA_ENDPOINT = "http://machines-data/all"
+SENSOR_DATA_ENDPOINT = "http://sensor-data-service/get_next_line"
 
 machines_list = requests.get(MACHINE_DATA_ENDPOINT)
 print(machines_list)
