a61f25c8a929e0c9607e5bf23da6823d1166951a
HigherGround189 <thehigherground18@gmail.com>
2026-02-10 15:48:08 +0800

    added sleep for exec debugging

--- src/model-inference-server/app/main.py
+++ src/model-inference-server/app/main.py
@@ -1,9 +1,11 @@
 import mlflow
 import requests
+from time import sleep
 
 MACHINE_DATA_ENDPOINT = "http://machines-data/all"
 SENSOR_DATA_ENDPOINT = "http://sensor-data-service/get_next_line"
 
 machines_list = requests.get(MACHINE_DATA_ENDPOINT)
 print(machines_list)
+sleep(200000000000)
 
