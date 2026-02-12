79625e92e13ba7ae111c4aa19795f892f5c19015
HigherGround189 <thehigherground18@gmail.com>
2026-02-12 23:58:49 +0800

    renamed logging.py to logging_setup.py to avoid import conflict

--- src/model-inference-server/app/main.py
+++ src/model-inference-server/app/main.py
@@ -3,13 +3,13 @@ import numpy as np
 from time import sleep
 import os
 import logging
-from app.logging import logging_setup
+from app.logging_setup import logging_setup
 
 logging_setup()
 logger = logging.getLogger(__name__)
 
 MODEL_NAME = os.getenv("MODEL_NAME")
-PREDICTION_INTERVAL = os.getenv("PREDICTION_INTERVAL")
+PREDICTION_INTERVAL = int(os.getenv("PREDICTION_INTERVAL"))
 
 model_uri = f"models:/{MODEL_NAME}/latest"
 model = mlflow.sklearn.load_model(model_uri)
