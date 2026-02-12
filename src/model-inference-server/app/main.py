7f12ed66b0b9036b4fc0c8db59efaed86c0ac055
HigherGround189 <thehigherground18@gmail.com>
2026-02-12 23:38:41 +0800

    removed fastapi from main

--- src/model-inference-server/app/main.py
+++ src/model-inference-server/app/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
 import mlflow
+import numpy as np
+from time import sleep
 import os
 
-mlflow.set_tracking_uri("http://mlflow.icantkube.help/")
+MODEL_NAME = os.getenv("MODEL_NAME")
 
-MODEL = os.getenv("MODEL_NAME")
-app = FastAPI(title=f"{MODEL} Inference Server", redirect_slashes=False)
+model_uri = f"models:/{MODEL_NAME}/latest"
+model = mlflow.sklearn.load_model(model_uri)
+print("Model loaded")
 
+while True:
+    X = np.array([[5.4, 3.4, 1.5]])
+    predictions = model.predict(X)
 
-@app.get("/inference/health")
-def health() -> dict[str, str]:
-    """Health check endpoint."""
-    return {"message": "health ok"}
+    print(f"Model Prediction: {predictions}")
+    sleep(5)
+
