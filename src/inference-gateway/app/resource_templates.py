dc0445a26e4d7e4bddeba169c791e5f6df6fdc1b
mysticshirou <hk.harishkanna@gmail.com>
2026-02-13 06:54:29 +0800

    Changed docker tage for model inference server in correct template

--- src/inference-gateway/app/resource_templates.py
+++ src/inference-gateway/app/resource_templates.py
@@ -25,7 +25,7 @@ def template_deployment(model_name: str, replicas: int, prediction_interval: str
             "containers": [
             {
                 "name": f"{model_name}-inference-server",
-                "image": "icantkube/model-inference-server:v0.24",
+                "image": "icantkube/model-inference-server:v0.26",
                 "ports": [
                 {
                     "containerPort": 80
