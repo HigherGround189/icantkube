d249a2a8a211cc2ba38c29476d9c7b4d59731a7c
HigherGround189 <thehigherground18@gmail.com>
2026-02-12 23:47:01 +0800

    changed print statements to logger instead

--- src/model-inference-server/app/main.py
+++ src/model-inference-server/app/main.py
@@ -2,17 +2,23 @@ import mlflow
 import numpy as np
 from time import sleep
 import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 MODEL_NAME = os.getenv("MODEL_NAME")
 
 model_uri = f"models:/{MODEL_NAME}/latest"
 model = mlflow.sklearn.load_model(model_uri)
-print("Model loaded")
+logger.info("Model loaded")
 
 while True:
     X = np.array([[5.4, 3.4, 1.5]])
     predictions = model.predict(X)
 
-    print(f"Model Prediction: {predictions}")
+    logger.info(f"Model Prediction: {predictions}")
     sleep(5)
-
