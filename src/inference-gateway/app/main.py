00082cf28d04d096db737ccd1369f0129dcd06a9
HigherGround189 <thehigherground18@gmail.com>
2026-02-13 18:44:05 +0800

    shifted code into for loop interator directly

--- src/inference-gateway/app/main.py
+++ src/inference-gateway/app/main.py
@@ -29,8 +29,7 @@ async def create_server(server: CreateServer):
 async def delete_server(server: DeleteServer):
     logger.info(server.model_name)
     check_if_deployment_exists()
-    deployment = kr8s.asyncio.get("deployment", f"{server.model_name.lower()}-inference-server", namespace=NAMESPACE)
-    async for deploy in deployment:
+    async for deploy in kr8s.asyncio.get("deployment", f"{server.model_name.lower()}-inference-server", namespace=NAMESPACE):
         print(dir(deploy), deploy)
         deploy.delete()
     
