dba0b2641f2e556f2846752c3d456744c01c4746
mysticshirou <hk.harishkanna@gmail.com>
2026-02-20 16:08:58 +0800

    Changed input array to exclude last element (target column)

--- src/model-inference-server/app/sensor_data.py
+++ src/model-inference-server/app/sensor_data.py
@@ -4,6 +4,6 @@ from app.constants import SENSOR_DATA_ENDPOINT
 def get_input_data():
     raw_input_data = requests.get(SENSOR_DATA_ENDPOINT, params={"name": "Wittman"})
     input = raw_input_data.json()["data"]
-    input_array = list(input.values())
+    input_array = list(input.values())[:-1]
 
     return input_array
